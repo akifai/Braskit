2236ac299db42298a41dbaebb52dbbae9792975f
Frank Usrs <frankusrs@gmail.com>
2013-09-30 17:06:32 +0200

    updated post model

--- inc/class.post.php
+++ inc/class.post.php
@@ -2,25 +2,31 @@
 defined('TINYIB') or exit;
 
 class Post {
+	public $globalid = 0;
+	public $id = 0;
 	public $parent = 0;
 	public $board = '';
 	public $timestamp = 0;
-	public $bumped = 0;
+	public $lastbump = 0;
+	public $ip = '127.0.0.2';
 	public $name = '';
 	public $tripcode = '';
 	public $email = '';
-	public $date = '';
 	public $subject = '';
 	public $comment = '';
 	public $password = '';
 	public $file = '';
 	public $md5 = '';
 	public $origname = '';
-	public $size = 0;
+	public $filesize = 0;
 	public $prettysize = '';
 	public $width = 0;
 	public $height = 0;
 	public $thumb = '';
 	public $t_width = 0;
 	public $t_height = 0;
+	public $date = '';
+	public $unixtime = 0;
+	public $banned = false;
+	public $reports = null;
 }
